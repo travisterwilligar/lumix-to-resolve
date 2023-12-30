8a8101a1c97b3e57eba85a2870b980af1946ec59
Travis T <travist@mini.local>
2023-12-30 14:45:19 -0700

    latest revision

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lumix-to-resolve
 Lumix to Davinci Resolve Meta Data import tool
+
+## Script to do
+* cleanly handle no input files
+
+### to do
+* what it does
+    * I extracts basic meta data from lumix video files such as aperture, iso, etc and encodes that metadata into a format davinci resolve can import.
+* dependencies
+    * exif tool
+    * python 3
+    * Mac and windows installer
+    * xmltodict - how to install (optional, but recommended)
+* how to install the script
+    * sudo cp getmetadata.py /usr/local/bin
+    * sudo chmod +x getmetadata.py
+* running the script
+    * go into the folder containing your videos and type getmetadata.py
+* importing metadata into resolve
+
--- getmetadata.py
+++ getmetadata.py
@@ -19,9 +19,14 @@ class Metadata:
         subprocess.Popen("exiftool -j -ext mov -ext mp4 . > " + self.inputFileName, shell=True, stdout=subprocess.PIPE).stdout.read()
 
     def load_exif(self):
-        with open(self.inputFileName) as f:
-            self.cameraMetaData = json.load(f)
-    
+        try:
+            with open(self.inputFileName) as f:
+                self.cameraMetaData = json.load(f)
+        except:
+            subprocess.Popen("rm  " + self.inputFileName, shell=True, stdout=subprocess.PIPE).stdout.read()
+            print('\N{cross mark}' + '\033[91m' + ' No lumix video files were scanned in this directory')
+            exit()
+
     def xml_to_json(self):
         if('PanasonicSemi-ProMetadataXml' in self.row):
             self.panasonic_xml_metadata = self.row['PanasonicSemi-ProMetadataXml']
@@ -35,21 +40,21 @@ class Metadata:
             self.row = row
             if(xmltodictFound != False):
                 self.row = self.xml_to_json()
-            self.rawMetaData.append(self.row)
-            print('\033[92m' + self.row['FileName'] + '\033[0m')
+                self.rawMetaData.append(self.row)
+                print('\033[92m' + u'\u2713' + self.row['FileName'] + '\033[0m')
 
     def map_to_resolve(self):
         for row in self.rawMetaData:
-            if(row['DynamicRangeBoost'] and row['DynamicRangeBoost'] == "On"):
-                row['ISO'] = str(row['ISO']) + " DR Boost On"
+            if(row.get('DynamicRangeBoost', False) != False):
+                if(row['DynamicRangeBoost'] == 'On'):
+                    row['ISO'] = str(row['ISO']) + " DR Boost On"
 
             #Resolve Mapping
             self.metadata = {
                 'File Name':                row['FileName'],
                 'ISO':                      row['ISO'],
                 'Shutter Speed':            row['ShutterSpeed'],
-                'Shutter Type':             'Speed',
-                'White Point (Kelvin)':     row['WhiteBalance'],
+                'White Point (Kelvin)':     row['ColorTempKelvin'],
                 'Shot Frame Rate':          row['VideoFrameRate'],
                 'Camera Type':              row['Model'], 
                 'Lens Type':                row['LensType'],             
@@ -58,12 +63,6 @@ class Metadata:
                 'Camera FPS':               row['VideoFrameRate'],
                 'Camera Serial #':          row['SerialNumber'],
                 'Camera Firmware':          row['FirmwareVersion'],
-                # 'Sample Rate (Khz)':        row['PanasonicSemi-ProMetadataXml']['ClipMain']['ClipContent']['EssenceList']['Audio']['SamplingRate'], 
-                # 'Bit Rate':                 row['PanasonicSemi-ProMetadataXml']['ClipMain']['ClipContent']['EssenceList']['Audio']['BitsPerSample'], 
-                # 'Gamma Notes':              row['PanasonicSemi-ProMetadataXml']['ClipMain']['UserArea']['AcquisitionMetadata']['CameraUnitMetadata']['Gamma']['CaptureGamma'], 
-                # 'Color Space Notes':        row['PanasonicSemi-ProMetadataXml']['ClipMain']['UserArea']['AcquisitionMetadata']['CameraUnitMetadata']['Gamut']['CaptureGamut'], 
-                # 'Codec Bitrate':            row['PanasonicSemi-ProMetadataXml']['ClipMain']['ClipContent']['EssenceList']['Video']['Codec']['@BitRate'],
-                # 'Camera Manufacturer':      row['PanasonicSemi-ProMetadataXml']['ClipMain']['ClipContent']['ClipMetadata']['Device']['Manufacturer'],
             }
             if(xmltodictFound != False):
                 self.metadata_extended = {
@@ -85,6 +84,9 @@ class Metadata:
                 w.writerow(row.values())
             print('Import metadata to resolve using ' + self.outputFileName)
 
+    def clean_up(self):
+        subprocess.Popen("rm  " + self.inputFileName, shell=True, stdout=subprocess.PIPE).stdout.read()
+
 def main():
     metadata = Metadata('input-exif2.json', 'resolve-metadata.csv')
     metadata.get_exif()
@@ -92,6 +94,7 @@ def main():
     metadata.process_exif()
     metadata.map_to_resolve()
     metadata.write_csv()
+    metadata.clean_up()
 
 if __name__ == "__main__":
     main()
